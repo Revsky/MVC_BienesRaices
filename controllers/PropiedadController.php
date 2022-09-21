5f05b0305e807745985d594ade93cf6f7bef0fb7
Revsky <angel1vengador100@gmail.com>
2022-09-21 12:23:46 -0500

    Mostrando las imagenes y propiedades en la ventana principal de admin, para que se muetsren las imagenes movemos la carpeta de imagenes a public

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -3,16 +3,22 @@
 namespace Controllers;
 
 use MVC\Router;
+use Model\Propiedad;
 
 class PropiedadController{
 
     public static function index(Router $router)
     {
+        $propiedades = Propiedad::all();
+        $resultado = null;
         /* 
         ** Mandamos en un arreglo el llave=>valor de las variables que crearemos para esa vista, estas nos ayudaran a enviar parametros para mostrar
+
+        ** Este arreglo define cada variable que estara 
         */
         $router->render('propiedades/admin',[
-            
+            'propiedades' => $propiedades,
+            'resultado' => $resultado,
         ]);
     }
 
--- views/propiedades/admin.php
+++ views/propiedades/admin.php
@@ -1,2 +1,60 @@
-<h1>Admin</h1>
-<?php echo $mensaje;?>
+
+<main class="contenedor seccion">
+    <h1>Administrador Bienes Raices CRUD</h1>
+
+    <?php 
+        if($resultado){
+            $mensaje = mostrarNotificacion(intval($resultado));
+            if($mensaje){?>
+                <p class="alerta exito"><?php echo sanitizar($mensaje)?></p>
+            <?php }
+        }
+    ?>
+
+    
+
+    <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+    <a href="/admin/vendedores/crear.php" class="boton boton-amarillo">Nuevo Vendedor</a>
+
+    <h2>Propiedades</h2>
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titutlo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody><!-- Mostrar los Resultados -->
+            <?php foreach( $propiedades as $propiedad ): ?>
+            <tr>
+                <td><?php echo $propiedad->id ?></td>
+                <td><?php echo $propiedad->titulo ?></td>
+                
+                <td><img src="/imagenes/<?php echo $propiedad->imagen?>" class="imagen-tabla"></td>
+                <td>
+                    <?php 
+                    
+                        $fmt = new NumberFormatter('mx_MX', NumberFormatter::CURRENCY);
+                        echo $fmt -> format( abs((int) $propiedad->precio));
+
+                    ?>
+                </td>
+                    <td>
+                    <form method="POST" class="w-100">
+
+                        <!-- Creamos un input que no se ve -->
+                        <input type="hidden" name="id" value=<?php echo $propiedad->id ?>>
+                        <input type="hidden" name="tipo" value="propiedad">
+                        <input type="submit" class="boton-rojo-block" value="Eliminar">
+                    </form>
+                    <a href="admin/propiedades/actualizar.php?id=<?php echo $propiedad->id ?>" class="boton-verde-block">Actualizar</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+
+</main>
