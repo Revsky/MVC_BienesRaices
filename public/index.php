e16b368c11fb71518027c264281aa06d0ee03a95
Revsky <angel1vengador100@gmail.com>
2022-09-23 10:36:06 -0500

    Creando URL para paginas principales

--- public/index.php
+++ public/index.php
@@ -7,6 +7,8 @@
     use MVC\Router;
     use Controllers\PropiedadController;
     use Controllers\VendedorController;
+    use Controllers\PaginasController;
+use FFI\ParserException;
 
     $router = new Router();
 
@@ -16,6 +18,8 @@
     /* 
     ** PropiedadController::class <- Define desde que clase viene desde ese namespace que en este caso es la clase PropiedadController, despues asignamos el método de esa clase que vamos a llamar el cual es index
     */
+
+    /* *** ZONA PRIVADA  *** */
     $router->get('/admin',[PropiedadController::class,'index']);
 
     // Propiedades
@@ -36,6 +40,17 @@
     
     $router->post('/vendedores/eliminar',[VendedorController::class,'eliminar']);
 
+    /* *** ZONA PUBLICA *** */
+
+    /* Sección para paginas principales */
+    $router->get('/',[PaginasController::class,'index']);
+    $router->get('/nosotros',[PaginasController::class,'nosotros']);
+    $router->get('/propiedades',[PaginasController::class,'propiedades']);
+    $router->get('/propiedad',[PaginasController::class,'propiedad']);
+    $router->get('/blog',[PaginasController::class,'blog']);
+    $router->get('/entrada',[PaginasController::class,'entrada']);
+    $router->get('/contacto',[PaginasController::class,'contacto']);
+    $router->post('/constacto',[PaginasController::class,'contacto']);/* <-- Permitira enviar emails */
 
 
     $router->comprobarRutas(); 
